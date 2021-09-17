5dc542ce7efe1d58b3a53494775aacb99cc47e6a
uprety <dipendraupreti37@gmail.com>
2021-09-17 09:07:16 +0545

    solved speeding

---
+++ speeding.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+pre_t, pre_d = 0, 0
+_ = input()
+
+previous_hi_speed = 0
+for _ in range(n -1):
+    t, d = map(int, input().split())
+    current_speed = int((d - pre_d)/(t - pre_t))
+    high_speed = max(current_speed, previous_hi_speed)
+    print(current_speed, previous_hi_speed, high_speed)
+    pre_t, pre_d = t, d
+    previous_speed = previous_hi_speed
+
+print(high_speed)
